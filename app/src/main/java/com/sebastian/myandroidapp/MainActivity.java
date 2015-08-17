a7fe35938743da2205e7d0b109157f18118858ec
ScholtenSeb <scholten.seb@gmail.com>
2015-08-18 00:28:28 +0200

    more shit

--- app/src/main/java/com/sebastian/myandroidapp/MainActivity.java
+++ app/src/main/java/com/sebastian/myandroidapp/MainActivity.java
@@ -1,12 +1,17 @@
 package com.sebastian.myandroidapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,6 +20,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                String loc = location.toString();
+                Toast.makeText(MainActivity.this,loc,Toast.LENGTH_LONG).show();
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+       // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
     public void launchNewMemory(View view) {
@@ -22,6 +48,9 @@ public class MainActivity extends ActionBarActivity {
         startActivity(intent);
     }
 
+    public void showLocation() {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/sebastian/myandroidapp/NewMemory.java
+++ app/src/main/java/com/sebastian/myandroidapp/NewMemory.java
@@ -1,11 +1,13 @@
 package com.sebastian.myandroidapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -13,63 +15,116 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 public class NewMemory extends ActionBarActivity {
 
     public ImageView mImageView;
+    public Uri imageUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_memory);
-
         mImageView = (ImageView) findViewById(R.id.imageView);
     }
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     public void launchCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-        }
+        startActivityForResult(intent, 10);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Uri mMediaUri  = data.getData();
-            String mMediaString = mMediaUri.toString();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ExifInterface exif = null;
-            try {
-                exif = new ExifInterface(mMediaString);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
-            int orientation = orientString != null ? Integer.parseInt(orientString) :  ExifInterface.ORIENTATION_NORMAL;
+        if (requestCode == 10) {
+            if (resultCode == RESULT_OK) {
+                imageUri = data.getData();
+                InputStream inputStream;
+                ExifInterface exifMedia = null;
 
-            int rotate = 0;
-            switch (orientation) {
-                case ExifInterface.ORIENTATION_ROTATE_270:
-                    rotate = 270;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_180:
-                    rotate = 180;
-                    break;
-                case ExifInterface.ORIENTATION_ROTATE_90:
-                    rotate = 90;
-                    break;
-            }
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+                    exifMedia = new ExifInterface(imageUri.getPath());
+                    Integer rotation = exifMedia.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    mImageView.setImageBitmap(RotateBitmap(bitmap,90));
+                    Toast.makeText(this,rotation.toString(),Toast.LENGTH_LONG).show();
 
-            Matrix matrix = new Matrix();
-            matrix.postRotate(rotate);
-            imageBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, imageBitmap.getWidth(), imageBitmap.getHeight(), matrix, true);
-            mImageView.setImageBitmap(imageBitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this,"Image not found",Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this,"Image not found",Toast.LENGTH_LONG).show();
+                }
+            }
         }
+    }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    public void saveMemory(View view) throws Exception {
+
+        if (imageUri != null) {
+
+            InputStream inputStream;
+            inputStream = new FileInputStream(new File(getPath(imageUri)));
+            byte[] data;
+            data = IOUtils.toByteArray(inputStream);
+            InputStreamBody inputStreamBody = new InputStreamBody(new ByteArrayInputStream(data),"image.jpg");
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://178.62.60.206:3000/memories/create");
+            String boundary = "-------------" + System.currentTimeMillis();
+            httpPost.setHeader("Content-type", "multipart/form-data; boundary="+boundary);
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+            builder.addPart("image", inputStreamBody);
+            httpPost.setEntity(builder.build());
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+
+            entity.consumeContent();
+            httpClient.getConnectionManager().shutdown();
+
+        } else {
+            Toast.makeText(this,"No image captured",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String getPath(Uri uri)
+    {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor == null) return null;
+        int column_index =             cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String s=cursor.getString(column_index);
+        cursor.close();
+        return s;
     }
 
     @Override
